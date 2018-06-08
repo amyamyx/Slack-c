27315953a9fd0fb986101f3102cc6ff7d914a8fd
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-06-08 10:47:58 -0700

    Add index to team's name

--- db/migrate/20180608164812_change_team_column.rb
+++ db/migrate/20180608164812_change_team_column.rb
@@ -1,4 +1,5 @@
 class ChangeTeamColumn < ActiveRecord::Migration[5.1]
   def change
+    add_index :teams, :name, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606030136) do
+ActiveRecord::Schema.define(version: 20180608164812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20180606030136) do
     t.string "img_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_teams_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
