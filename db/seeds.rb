802c1a3a58dbe85244da2e08197b3f241e1195ff
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-06-11 17:06:22 -0700

    Seed team

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Team.create(name: "Team1", creator_id: 1)
