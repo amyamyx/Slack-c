3e615ffbf44671b142fd50e6a3bf25eacc9b4a49
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-06-15 09:51:03 -0700

    Fix logout button

--- frontend/components/main_page/sidebar/sidebar.jsx
+++ frontend/components/main_page/sidebar/sidebar.jsx
@@ -21,7 +21,7 @@ class Sidebar extends React.Component {
       directId, 
       channelId, 
       currentUser,
-      logout, 
+      logout,
       closeModal
      } = this.props;
 
@@ -32,7 +32,7 @@ class Sidebar extends React.Component {
           <div>
             <h3 className="team-name">{team.name}</h3>
             <span>{currentUser.username}</span>
-            <button onClick={() => handleClick()}>logout</button>
+            <button onClick={() => this.handleClick()}>logout</button>
           </div>
         </div>
 
